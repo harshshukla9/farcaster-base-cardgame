6e1f58d86e4f9eb823d9e622b716da0d69b5371e
Rahul Roy <rroy1836@gmail.com>
2025-10-17 23:46:52 +0530

    Update Manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -7,14 +7,18 @@ export async function GET() {
       "name": "Base Stack",
       "version": "1",
       "iconUrl": "https://farcaster-base-cardgame-gilt.vercel.app/icon.png",
-      "homeUrl": "https://farcaster-base-cardgame-gilt.vercel.app/",
+      "homeUrl": "https://farcaster-base-cardgame-gilt.vercel.app",
       "imageUrl": "https://farcaster-base-cardgame-gilt.vercel.app/image.png",
       "splashImageUrl": "https://farcaster-base-cardgame-gilt.vercel.app/splash.png",
       "splashBackgroundColor": "#ffffff",
       "webhookUrl": "https://farcaster-base-cardgame-gilt.vercel.app/api/webhook",
       "subtitle": "Match 3 same cards",
-      "description": "Match 3 same cards",
-      "primaryCategory": "games"
+      "description": "Match 3 same cards to clear them all",
+      "primaryCategory": "games",
+      "screenshotUrls": [
+        "https://farcaster-base-cardgame-gilt.vercel.app/image.png"
+      ],
+      "heroImageUrl": "https://farcaster-base-cardgame-gilt.vercel.app/image.png"
     },
     "accountAssociation": {
       "header": "eyJmaWQiOjExMjAzODgsInR5cGUiOiJhdXRoIiwia2V5IjoiMHg1NDMwYWNGMENEMEI5RTNiMmFCMkQ5NDJBZjM0MzJCQTJmYzJFNGY3In0",
