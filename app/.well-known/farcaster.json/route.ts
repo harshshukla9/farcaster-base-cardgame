86ca932d45e7b7d5c6626911581335b81bbeef6d
harshshukla9 <harshshukla9792@gmail.com>
2025-10-17 23:53:44 +0530

    url fix

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -6,9 +6,9 @@ export async function GET() {
     "frame": {
       "name": "Base Stack",
       "version": "1",
-      "iconUrl": "https://farcaster-base-cardgame-gilt.vercel.app/icon.png",
+      "iconUrl": "https://farcaster-base-cardgame-gilt.vercel.app/splash.png",
       "homeUrl": "https://farcaster-base-cardgame-gilt.vercel.app",
-      "imageUrl": "https://farcaster-base-cardgame-gilt.vercel.app/image.png",
+      "imageUrl": "https://farcaster-base-cardgame-gilt.vercel.app/splash.png",
       "splashImageUrl": "https://farcaster-base-cardgame-gilt.vercel.app/splash.png",
       "splashBackgroundColor": "#ffffff",
       "webhookUrl": "https://farcaster-base-cardgame-gilt.vercel.app/api/webhook",
@@ -16,9 +16,9 @@ export async function GET() {
       "description": "Match 3 same cards to clear them all",
       "primaryCategory": "games",
       "screenshotUrls": [
-        "https://farcaster-base-cardgame-gilt.vercel.app/image.png"
+        "https://farcaster-base-cardgame-gilt.vercel.app/splash.png"
       ],
-      "heroImageUrl": "https://farcaster-base-cardgame-gilt.vercel.app/image.png"
+      "heroImageUrl": "https://farcaster-base-cardgame-gilt.vercel.app/splash.png"
     },
     "accountAssociation": {
       "header": "eyJmaWQiOjExMjAzODgsInR5cGUiOiJhdXRoIiwia2V5IjoiMHg1NDMwYWNGMENEMEI5RTNiMmFCMkQ5NDJBZjM0MzJCQTJmYzJFNGY3In0",
